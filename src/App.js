2274dcca211d81d38a4ab8d1e5b64f107da935f1
Eric Willits <ericwillits@Erics-MacBook-Pro.local>
2022-01-21 01:32:37 -0800

    build second try

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import { Amplify } from 'aws-amplify';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
 
-import { Authenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-
-import awsExports from './aws-exports';
-Amplify.configure(awsExports);
-
-export default function App() {
+function App() {
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <main>
-          <h1>Hello {user.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-        </main>
-      )}
-    </Authenticator>
+    <div className="App">
+      <header>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>We now have Auth!</h1>
+      </header>
+      <AmplifySignOut />
+    </div>
   );
 }
+
+export default withAuthenticator(App);
